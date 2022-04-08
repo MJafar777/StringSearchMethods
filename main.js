b675eaa032e12101b9740ebcc186868b32bc5376
MJafar777 <94430558+MJafar777@users.noreply.github.com>
2022-04-08 07:34:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// string search Methods : indexOf() , LastIndexOf() , includes() , startWidth() , endWidth
+
+let str = "hello world lorem ipsum dolor amet ipsum";
+
+// let res = str.indexOf("world"); // nechinchi indexdaligini tekshiradi
+let res = str.IndexOf("ipsum");
+console.log(res);
